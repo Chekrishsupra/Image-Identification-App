0037b9cd5dd5398bcabb9c8794e02007452c4ff2
Chekrishsuper <78082844+Chekrishsupra@users.noreply.github.com>
2021-07-07 19:34:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,3 +9,26 @@ Webcam.set({
 });
 camera = document.getElementById("camera");
 Webcam.attach("#camera");
+function take_picture() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = "<img id='captured_image' src='"+data_uri+"'>";
+    });
+}
+console.log("Ml5 version:",ml5.version);
+classifier = ml5.imageClassifier('MobileNet',ModelLoaded);
+function ModelLoaded() {
+    console.log("Model is loaded");
+}
+function check() {
+    img = document.getElementById("captured_image");
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,results) {
+    if(error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+    }
+}
